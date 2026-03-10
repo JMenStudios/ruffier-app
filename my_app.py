f9d8e172ca5599cf331d2a9b6510c9382df1b6c7
JMenStudios <jmenstudiosoficial@gmail.com>
2026-03-10 17:28:40 -0500

    Add files via upload
    nuevos archivos

--- my_app.py
+++ my_app.py
@@ -1,58 +1,70 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # comprobar los tipos de los valores de entrada
 from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
 
 from instr import *
 from second_win import *
-
-       
+     
 class MainWin(QWidget):
-    def __init__(self):
-        ''' la ubicacion de la ventana principal '''
-        super().__init__()
+   def __init__(self):
+       ''' la ventana en donde se encuentra el saludo  '''
+       super().__init__()
 
-        # crea y configura los elementos graficos:
-        self.initUI()
 
-        # establece conexion con los distintos elementos
-        self.connects()
+       # establece la apariencia de la ventana (etiqueta, tamaño, ubicación)
+       self.set_appear()
 
-        # dicta como se tiene que visualizar la ventana (label, tamaño, localización)
-        self.set_appear()
 
-        # empezar:
-        self.show()
+       # creando y configurando elementos gráficos
+       self.initUI()
 
-    def initUI(self):
-        ''' crea los elementos graficos'''
-        self.btn_next = QPushButton(txt_next, self)
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
 
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft) 
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)          
-        self.setLayout(self.layout_line)
+       #establece la conexión entre los elementos
+       self.connects()
 
-    
-    def next_click(self):
-        self.tw = TestWin()
-        self.hide()
 
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
+       # inicio:
+       self.show()
 
-    ''' dicta como se tiene que visualizar la ventana (label, tamaño, localización) '''
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
 
-app = QApplication([])
-mw = MainWin()
-app.exec_()
+   def initUI(self):
+       ''' crea elementos gráficos '''
+       self.btn_next = QPushButton(txt_next)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
 
+
+       self.layout = QVBoxLayout()
+       self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout)
+
+
+  
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
+   def connects(self):
+       self.btn_next.clicked.connect(self.next_click)
+
+
+   ''' establece la apariencia de la ventana (etiqueta, tamaño, ubicación) '''
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+
+
+def main():
+   app = QApplication([])
+   mw = MainWin()
+   app.exec_()
+
+
+main()
